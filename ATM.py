96bdbe92826c8f87624c89a718a9aa0347b6eb67
E-L <45451697+ekow-lloyd@users.noreply.github.com>
2021-04-21 20:38:41 +0200

    Update ATM.py
    fixed the bug where program kept exiting after loop

--- ATM.py
+++ ATM.py
@@ -46,21 +46,32 @@ while attempts != 0:
 def transaction():
     print()
     print('what transaction do you want to perform ? \n')
-    options = pyip.inputMenu(['Deposit', 'Withdrawal'],lettered=False, numbered=True)
+    options = pyip.inputMenu(['Deposit', 'Withdrawal', 'Balance Enquiry'],lettered=False, numbered=True)
     if options == 'Deposit':
         print("kindly put the notes in the tray \n\n***Processing*** \n\nDeposit completed \n\n")
+        print("Do you want to perform other transactions ?\n")
+        done=pyip.inputMenu(["Yes","No"],lettered=False,numbered=True)
+        if done == 'No':
+        pass
     if options == 'Withdrawal':
         print("how much would you like to witdraw ?( in multiples of 10)\n\n")
         cashout = pyip.inputInt('enter amount: ')
         print('{} dispensed, have a nice day {}'.format(cashout,client))
+        print("Do you want to perform other transactions ?\n")
+        done=pyip.inputMenu(["Yes","No"],lettered=False,numbered=True)
+        if done == 'No':
+        pass
+    if options == 'Balance Enquiry':
+        print('Current Account - €1980\nSavings Account - €50,000')
+        print("Do you want to perform other transactions ?\n")
+        done=pyip.inputMenu(["Yes","No"],lettered=False,numbered=True)
+        if done == 'No':
+        pass
+    else:
+        transaction()
 transaction()
 
-print("Do you want to perform another transaction ?\n")
-done=pyip.inputMenu(["Yes","No"],lettered=False,numbered=True)
-if done == 'No':
-    pass
-else:
-    transaction()
+
     
 print("thanks for banking with us\n")    
         
